b5d491e10186a2e964ee872cd7b83e43c2704b99
vigneshy2023F <vigneshy@vt.edu>
2024-02-04 11:23:33 -0500

    ui

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
-// import logo from './logo.svg';
-// import './App.css';
 import { useState, useEffect } from "react";
 import "./App.css";
 import "./main.css";
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         Deployment is happening here
-//       </header>
-//     </div>
-//   );
-// }
+
 
 function App(){
   const[data, setData] = useState([]);
   const[loading, setLoading] = useState(false);
+  const [expandedItems, setExpandedItems] = useState({});
 
- useEffect(()=>{
-//   fetch("https://jsonplaceholder.typicode.com/posts").then((response)=>response.json()).then((json)=>setData(json));
- }, [])
+
+useEffect(() => {
+  // Fetch data on component mount
+  getListings();
+}, []);
+//  useEffect(()=>{
+// //   fetch("https://jsonplaceholder.typicode.com/posts").then((response)=>response.json()).then((json)=>setData(json));
+//  }, [])
+
+
+
 
 function getListings(){
   setLoading(true);
   setTimeout(()=>{
     setLoading(false);
   }, 2000);
-//setData == console.log
-  //fetch("http://localhost:8080/tenants", {mode: 'no-cors'}).then((response)=>response.json()).then((json)=>setData(json));
-  fetch("http://localhost:8080/tenants", {method: "GET"})
+  fetch("http://localhost:8080/listings", {method: "GET"})
     .then((response)=>response.json())
     .then((json)=>{
       console.log(json);
-      console.log(json._embedded.tenantList);
-      setData(json._embedded.tenantList);
+      console.log(json._embedded.listingList);
+      setData(json._embedded.listingList);
     });
-  //fetch("https://jsonplaceholder.typicode.com/posts").then((response)=>response.json()).then((json)=>setData(json));
 }
+
+
+
 
 // function getTenants(){
 //   setLoading(true);
@@ -50,36 +48,68 @@ function getListings(){
 //   fetch("https://jsonplaceholder.typicode.com/posts").then((response)=>response.json()).then((json)=>setData(json));
 // }
 
+
+
+
 function getLogin(){
-  alert("sign in");
+  alert("Signed in!");
 }
-function expand(){
-  
+
+
+function toggleItem(listingId) {
+  setExpandedItems((prevExpandedItems) => ({
+    ...prevExpandedItems,
+    [listingId]: !prevExpandedItems[listingId],
+  }));
 }
+
+
+
 
   return (
     <div className="container">
-  
+ 
   <button className="btn" onClick={getLogin}>Sign in</button>
+
 
 
 
   <h1 className="title">Your Next Nest</h1>
 
+
+
+
   <button className="btn" onClick={getListings}>For Renters: Get Listings</button>
 
+
+
+
   {/* <button className="btn" onClick={getTenants}>For Leasers: Get Tenants</button> */}
-  
-  {loading ? <h2 className="mt-2">LOADING...</h2> :  ( <ul>{data.map((post)=>(<li className="alert alert-info" key={post.id}>
-    {post.id}{post.name}
-  </li>))}</ul>)}
-
-  
-  
-
-  </div>
-  
+  {loading ? (
+        <h2 className="mt-2">LOADING...</h2>
+      ) : (
+        <ul>
+          {data.map((listing) => (
+            <li className="alert alert-info" key={listing.id}>
+              <div onClick={() => toggleItem(listing.id)}>
+                {listing.id}. {listing.name}
+              </div>
+              {expandedItems[listing.id] && (
+                <div>
+                  <p>
+                  {listing.address}<br></br>
+                  </p>
+                  {listing.address}
+                </div>
+              )}
+              <button onClick={() => toggleItem(listing.id)}>
+                {expandedItems[listing.id] ? "-" : "+"}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
-
 export default App;
